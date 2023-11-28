1da34603007fae997905b76ea08781b4dc783cc7
swngo <scottwngo@gmail.com>
2023-11-28 15:43:13 -0800

    r ordered imports

--- src/modules/Course/index.tsx
+++ src/modules/Course/index.tsx
@@ -1,7 +1,8 @@
+import { ReactElement } from "react";
+
 import Content from "./Content";
 import Header from "./Header";
 import Total from "./Total";
-import { ReactElement } from "react";
 
 export interface IPart {
   id: number;
