44a21b906615f1400093213edc009d3763a674f1
swngo <scottwngo@gmail.com>
2023-11-29 13:49:36 -0800

    F simplified person.service.ts

--- src/services/person.service.ts
+++ src/services/person.service.ts
@@ -1,49 +1,22 @@
-import { AxiosError } from "axios";
-import { useEffect, useState } from "react";
-import axios, { isAxiosError } from "axios";
-
-export interface ValidationError {
-  message: null | string;
-  errors: null | Record<string, string[]>;
-}
-
-export interface ApiResponse {
-  loading: boolean;
-  data: any;
-  error: AxiosError<ValidationError, Record<string, unknown>> | null;
-}
+import axios from "axios";
+import { IPerson } from "@/modules/PhoneBook";
 
 const baseUrl = "http://localhost:3001/persons";
 
-const useFetchPersons = (): ApiResponse => {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
-  const [error, setError] = useState<null | AxiosError<
-    ValidationError,
-    Record<string, unknown>
-  >>(null);
-
-  useEffect(() => {
-    setLoading(true);
-    (async () => {
-      try {
-        const resp = await axios.get(baseUrl);
-        const data = await resp?.data;
-
-        setData(data);
-      } catch (error: unknown) {
-        if (isAxiosError<ValidationError, Record<string, unknown>>(error)) {
-          setError(error);
-        } else {
-          console.error(error);
-        }
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, []);
-
-  return { loading, data, error };
+const fetchPersons = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
-export default { useFetchPersons };
+const createPerson = async (person: Partial<IPerson>) => {
+  const response = await axios.post(baseUrl, person);
+  return response.data;
+};
+
+const deletePerson = async (id: number) => {
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  console.log("response.data", response.data);
+  return response.data;
+};
+
+export default { createPerson, deletePerson, fetchPersons };
