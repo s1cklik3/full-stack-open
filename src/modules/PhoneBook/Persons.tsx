8ad0380016f438fbda48804ddc2f47d83cdb1f5a
swngo <scottwngo@gmail.com>
2023-11-29 13:52:58 -0800

    F added delete person

--- src/modules/PhoneBook/Persons.tsx
+++ src/modules/PhoneBook/Persons.tsx
@@ -1,14 +1,41 @@
-import { ReactElement } from "react";
+import { Dispatch, ReactElement, SetStateAction, useState } from "react";
 
 import { IPerson } from "./index";
+import PersonService from "@services/person.service";
 
-const Person = ({ persons }: { persons: IPerson[] | undefined }): ReactElement => {
+const Person = ({
+  persons,
+  setPersons,
+}: {
+  persons: IPerson[];
+  setPersons: Dispatch<SetStateAction<IPerson[]>>;
+}): ReactElement => {
+  const [error, setError] = useState(null);
+  const handleDeletePerson = (person: IPerson): void => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      PersonService.deletePerson(person.id)
+        .then(() => {
+          setPersons([
+            ...persons.filter(
+              (deletedPerson: IPerson) => deletedPerson.id !== person.id,
+            ),
+          ]);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    }
+  };
+
+  if (error) return <></>;
+
   return (
     <>
       <h2>Numbers</h2>
-      {persons?.map((person) => (
+      {persons?.map((person: IPerson) => (
         <li key={person.id}>
           {person.name} {person.number}
+          <button onClick={() => handleDeletePerson(person)}>delete</button>
         </li>
       ))}
     </>
