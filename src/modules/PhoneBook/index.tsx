064c48451a347af998b7a82f6c5418c2fc8d49f2
swngo <scottwngo@gmail.com>
2023-12-07 13:30:20 -0800

    F using Error and Notification components

--- src/modules/PhoneBook/index.tsx
+++ src/modules/PhoneBook/index.tsx
@@ -5,6 +5,9 @@ import PersonService from "@services/person.service";
 import PersonFilter from "./PersonFilter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import Notification from "@/modules/PhoneBook/Notification";
+import Error from "@/modules/PhoneBook/Error";
+import { AxiosResponse } from "axios";
 
 export interface IPerson {
   id: number;
@@ -13,24 +16,30 @@ export interface IPerson {
 }
 
 const PhoneBook = (): ReactElement => {
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
   const [persons, setPersons] = useState<IPerson[]>([]);
+  const [notification, setNotification] = useState<string | null>(null);
 
   useEffect(() => {
     PersonService.fetchPersons()
-      .then((persons: IPerson[]) => setPersons(persons))
+      .then(({ data: persons }: AxiosResponse) => setPersons(persons))
       .catch((error) => {
         setError(error);
       });
   }, []);
 
-  if (error) return <></>;
-
   return (
     <>
       <h2>Phonebook</h2>
+      <Error message={error}></Error>
+      <Notification message={notification}></Notification>
       <PersonFilter persons={persons} setPersons={setPersons}></PersonFilter>
-      <PersonForm persons={persons} setPersons={setPersons}></PersonForm>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        setNotification={setNotification}
+        setError={setError}
+      ></PersonForm>
       <Persons persons={persons} setPersons={setPersons}></Persons>
     </>
   );
