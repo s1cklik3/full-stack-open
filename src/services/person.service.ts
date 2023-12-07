2b5ccec5dbf6923cefc3fa2a4d5d6e6feb82c6df
swngo <scottwngo@gmail.com>
2023-12-07 13:29:29 -0800

    R delegated handling of requests to components

--- src/services/person.service.ts
+++ src/services/person.service.ts
@@ -3,24 +3,14 @@ import { IPerson } from "@/modules/PhoneBook";
 
 const baseUrl = "http://localhost:3001/persons";
 
-const fetchPersons = async () => {
-  const response = await axios.get(baseUrl);
-  return response.data;
-};
+const fetchPersons = async () => await axios.get(baseUrl);
 
-const createPerson = async (person: Partial<IPerson>) => {
-  const response = await axios.post(baseUrl, person);
-  return response.data;
-};
+const createPerson = async (person: Partial<IPerson>) =>
+  await axios.post(baseUrl, person);
+const deletePerson = async (id: number) =>
+  await axios.delete(`${baseUrl}/${id}`);
 
-const deletePerson = async (id: number) => {
-  const response = await axios.delete(`${baseUrl}/${id}`);
-  return response.data;
-};
-
-const updatePerson = async (person: Partial<IPerson>) => {
-  const response = await axios.put(`${baseUrl}/${person.id}`, person);
-  return response.data;
-};
+const updatePerson = async (person: Partial<IPerson>) =>
+  await axios.put(`${baseUrl}/${person.id}`, person);
 
 export default { createPerson, deletePerson, fetchPersons, updatePerson };
