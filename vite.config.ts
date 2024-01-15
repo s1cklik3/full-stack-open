2dd303a22262b11b18b64a7a12da6277efd4a8ca
swngo <scottwngo@gmail.com>
2024-01-15 10:40:12 -0800

    E added vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,4 +13,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    testMatch: ['./**/*.spec.tsx'],
+  },
+}));
